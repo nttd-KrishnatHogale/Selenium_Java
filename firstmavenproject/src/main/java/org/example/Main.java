744b6c8d990eb70dbcc71502bcd93b64968941a1
Krishnat-Hogale_frgr <Krishnat.Hogale@nttdata.com>
2024-07-17 13:05:52 +0000

    first project added

---
+++ firstmavenproject/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+//        ChromeDriver driver = new ChromeDriver();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/");
+        String actualTitl = driver.getTitle();
+        String expectedTitl = "nopCommerce demo store";
+        if (actualTitl.equals(expectedTitl)){
+            System.out.println("passed");
+        }
+        else {
+            System.out.println("failed");
+        }
+
+        driver.quit();
+    }
+}
