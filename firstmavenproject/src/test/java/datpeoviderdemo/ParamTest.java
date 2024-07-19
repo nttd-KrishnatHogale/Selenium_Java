fca98d7fbf3e8c8f640ed4c9e5bcf9901917b178
Krishnat-Hogale_frgr <Krishnat.Hogale@nttdata.com>
2024-07-19 11:32:57 +0000

    serail execution using xml file

--- firstmavenproject/src/test/java/datpeoviderdemo/ParamTest.java
+++ firstmavenproject/src/test/java/datpeoviderdemo/ParamTest.java
@@ -5,20 +5,39 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import javax.sound.midi.Soundbank;
 import java.time.Duration;
+
 public class ParamTest {
     private WebDriver driver;
 
     @BeforeClass
-    void setup() throws InterruptedException{
-        driver = new ChromeDriver();
+    @Parameters({"browser"})
+    void setup(String br) throws InterruptedException {
+
+        switch (br.toLowerCase()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                System.out.println("Invalid Browser");
+                return;
+
+
+        }
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().window().maximize();
@@ -27,24 +46,27 @@ public class ParamTest {
     }
 
     @Test(priority = 1)
-    void testLogo(){
+    void testLogo() {
         boolean status = driver.findElement(By.xpath("//img[@alt='company-branding']")).isDisplayed();
-        Assert.assertEquals(status,true);
+        Assert.assertEquals(status, true);
 
     }
+
     @Test(priority = 2)
-    void testTitle(){
-        Assert.assertEquals(driver.getTitle(),"OrangeHRM");
+    void testTitle() {
+        Assert.assertEquals(driver.getTitle(), "OrangeHRM");
 
     }
+
     @Test(priority = 3)
-    void testUrl(){
-        Assert.assertEquals(driver.getCurrentUrl(),"https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    void testUrl() {
+        Assert.assertEquals(driver.getCurrentUrl(), "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
 
     }
+
     @AfterClass
-    void tearDown(){
+    void tearDown() {
         driver.quit();
 
     }
