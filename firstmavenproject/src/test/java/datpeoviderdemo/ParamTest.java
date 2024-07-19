b82448cc9ac8356067042fac6f5370e6f9be3a5e
Krishnat-Hogale_frgr <Krishnat.Hogale@nttdata.com>
2024-07-19 10:48:22 +0000

    Crated Xml File

--- firstmavenproject/src/test/java/datpeoviderdemo/ParamTest.java
+++ firstmavenproject/src/test/java/datpeoviderdemo/ParamTest.java
@@ -1,5 +1,52 @@
 package datpeoviderdemo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 public class ParamTest {
+    private WebDriver driver;
+
+    @BeforeClass
+    void setup() throws InterruptedException{
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+
+    }
+
+    @Test(priority = 1)
+    void testLogo(){
+        boolean status = driver.findElement(By.xpath("//img[@alt='company-branding']")).isDisplayed();
+        Assert.assertEquals(status,true);
+
+    }
+    @Test(priority = 2)
+    void testTitle(){
+        Assert.assertEquals(driver.getTitle(),"OrangeHRM");
+
+    }
+    @Test(priority = 3)
+    void testUrl(){
+        Assert.assertEquals(driver.getCurrentUrl(),"https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+
+    }
+    @AfterClass
+    void tearDown(){
+        driver.quit();
+
+    }
 
 }
