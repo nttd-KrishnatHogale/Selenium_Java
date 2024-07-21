f29eab4e3663e1750cacb2cfe8a6bb6dd12bbd15
Krishnat-Hogale_frgr <Krishnat.Hogale@nttdata.com>
2024-07-21 05:49:14 +0000

    How class

--- firstmavenproject/src/test/java/pageobjectmodelpattern/LoginPage2.java
+++ firstmavenproject/src/test/java/pageobjectmodelpattern/LoginPage2.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
@@ -15,18 +16,20 @@ public class LoginPage2 {
     LoginPage2(WebDriver driver) {
 
         this.driver = driver;
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
 
     //locatiors
 //    By txt_username_loc = By.xpath("//input[@placeholder='Username']");
 //    By txt_password_loc = By.xpath("//input[@placeholder='Password']");
 //    By btn_login_loc = By.xpath("//button[normalize-space()='Login']");
-    @FindBy(xpath="//input[@placeholder='Username']")
+//    @FindBy(xpath="//input[@placeholder='Username']")
+//    WebElement txt_username;
+    @FindBy(how = How.XPATH, using = "//input[@placeholder='Username']")
     WebElement txt_username;
-    @FindBy(xpath="//input[@placeholder='Password']")
+    @FindBy(xpath = "//input[@placeholder='Password']")
     WebElement txt_password;
-    @FindBy(xpath="//button[normalize-space()='Login']")
+    @FindBy(xpath = "//button[normalize-space()='Login']")
     WebElement btn_login;
 
 //    @FindBy(tagName = "a")
@@ -39,8 +42,10 @@ public class LoginPage2 {
     }
 
     public void setPassword(String pwd) {
-    txt_password.sendKeys(pwd);    }
+        txt_password.sendKeys(pwd);
+    }
 
     public void clickLogin() {
-btn_login.click();    }
+        btn_login.click();
+    }
 }
